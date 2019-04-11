2f391cfc2dceb1c9b4db7e7610f98a17db5cdc51
berry <48694358+berrytu@users.noreply.github.com>
2019-04-11 22:25:19 +0200

    Update freecodecamp_solutions.m

--- freecodecamp_solutions.m
+++ freecodecamp_solutions.m
@@ -1,2 +1,66 @@
 // style
 /*style*/
+
+var myName ;
+
+var a = 7;
+var b = 7;
+var b = a;
+
+var a = 9;
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+a = 6;
+b = 15;
+c = "I am a String!"
+
+var studlyCapVar ;
+var properCamelCase ;
+var titleCaseOver ;
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+var sum = 10 + 10;
+
+var difference = 45 - 33;
+
+var product = 8 * 10;
+
+var quotient = 66 / 33;
+
+ myVar++;
+ 
+ myVar--;
+ 
+ var myDecimal = 5.7;
+ 
+ var product = 2.0 * 2.5;
+ 
+ var quotient = 4.4 / 2.0; // Fix this line
+ 
+ var remainder = 11 % 3;
+ 
+ a += 12;
+b += 9;
+c += 7;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+var myFirstName = "berry";
+var myLastName = "kiflit" ;
+
+"I am a \"double quoted\" string inside \"double quotes\".";
+
